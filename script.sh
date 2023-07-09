1bf22da2502318dc6a98159a67301422fb0224a3
alemd01 <aaleemd11@gmail.com>
2023-07-09 21:27:39 +0200

    cambios

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 
 #genero la página estática con los cambios.
-cd /home/alemd/Github/alemd-11ty-11r/
+cd /home/usuario/eleventy/alemd-11ty-11r/
 npm run build
 #copio la página estática generada al repositorio github
-cp -r /home/alemd/Github/alemd-11ty-11r/dist/* /home/alemd/Github/alemd-cutreblog-11r
-cd /home/alemd/Github/alemd-cutreblog-11r
+cp -r /home/usuario/eleventy/alemd-11ty-11r/dist/* /home/alemd/Github/alemd-cutreblog-11r
+cd /home/usuario/eleventy/alemd-cutreblog-11r
 
 #Realizamos los cambios en github
 git add .
